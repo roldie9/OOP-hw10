c693b42d24a301947a7e221285ae0a8b64aa6148
roldie9 <aroldoreyes9@gmail.com>
2020-02-27 23:22:34 -0600

    can't get app.js to populate without errors

--- Master/app.js
+++ Master/app.js
@@ -4,15 +4,12 @@ const intern = require("./lib/intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
-// const OUTPUT_DIR = path.resolve(__dirname, "output")
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/htmlRenderer");
 ​
-// const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 inquirer.prompt ([
     {
@@ -20,7 +17,7 @@ inquirer.prompt ([
         message: "Which type of team member would you like to add?",
         name: "role",
         choices: ["manager", "engineer", "intern"]
-    },
+    }
    
 ]).then(function(answer) {
 
@@ -36,17 +33,16 @@ inquirer.prompt ([
         answer.role = intern
     }
 
-    // fs.writeFile("Master\templates\main.html", answer, function() {
-
+    fs.writeFile("Master\templates\main.html", answer, function() {
     });
 });
 ​
-module.exports = team;
+module.exports = employee;
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+
+const team = $.templates("team.html");
+
+const html = team.render({
+});
+
+$("#employeeName").html(html);
